2510abf22f70be1036b4670c41a9eb1604f01f9e
DeNam2 <namnguyen55555666@gmail.com>
2025-04-18 22:46:03 +0700

    update code-run

--- GPIO/Bài 1/main.c
+++ GPIO/Bài 1/main.c
@@ -7,7 +7,7 @@ void init_RCC(void);
 void GPIO_config(void);
 
 uint32_t LED[10] = {0x01F80200, 0x003003C8, 0x02D80120, 0x02780180, 0x033000C8, 
-								0x03680090, 0x03E80010, 0x003803C0, 0x03F80000, 0x03780080};
+								0x03680090, 0x03E80010, 0x003803C0, 0x03F80000, 0x03780080};								//mang hien so tren led tu 0-9 nhung dung thanh ghi BSRR
 
 int main(void)
 {
@@ -15,7 +15,28 @@ int main(void)
 	GPIO_config();
 	while(1)
 	{
-		
+		for (uint32_t i = 0; i < 100; i++)
+		{
+			for (uint32_t j = 0; j < 100; j++)     //tao delay 400ms de hien thi du cho nguoi nhin duoc thông so led hien là bao nhiêu
+			{
+				//Bât led hang chuc truoc
+				GPIOB->BSRR = LED[i/10];
+				GPIOA->BSRR = 0x20001;  //A0 la hang chuc
+				delay(2);
+				//GPIOA->BSRR = 0x30000;
+				GPIOA->BRR = 0x03;
+				//Led hang don vi
+				GPIOB->BSRR = LED[i%10];
+				GPIOA->BSRR = 0x10002;   //A1 la hang don vi
+				delay(2);
+				//GPIOA->BSRR |= 0x30000;
+				GPIOA->BRR = 0x03;
+			}
+		}
+//		GPIOA->ODR = 0x02;
+//		delay(10);
+//		GPIOB->ODR = 0x0200;
+//		delay(10);
 	}
 }
 
@@ -30,10 +51,14 @@ void delay(uint16_t time)
 void init_RCC()
 {
 	RCC->APB2ENR |= 0x0C;
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 }
 
 void GPIO_config()
 {
+	//Chan PB3 và PB4 default là JTAB debug
+	AFIO->MAPR &= ~(0x7 << 24);  // Xóa bits liên quan JTAG
+	AFIO->MAPR |= (0x2 << 24);   // Select mode "JTAG-DISABLE, SWD-ENABLE"
 	//Config portB
 	GPIOB->CRL &= ~(0xFFFFF000);  //reset chân 3,4,5,6,7
 	GPIOB->CRL |= 0x33333000;     //configure chân 3,4,5,6,7 là mode output push-pull, speed 50MHz
@@ -43,5 +68,3 @@ void GPIO_config()
 	GPIOA->CRL &= ~(0x00FF);      //reset chân 0,1
 	GPIOA->CRL |= 0x0033;					//configure chân 0,1 là mode output push-pull, speed 50MHz
 }
-
-
